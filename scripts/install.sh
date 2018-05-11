4ac77e63c1167caf97ceb002608ad063d926d7e6
amine-at <39156201+amine-at@users.noreply.github.com>
2018-05-11 16:07:26 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install -y nginx docker.io vim lvm2
+sudo chmod 777 /var/run/docker.sock
+docker run -d -p 8080:8080 dockuwiki:v1
 sudo apt-get install awscli -y
 sudo apt-get install tmux -y
