50f39b23b063ed44ab2d40244edc4aac6d79b741
Ubuntu <ubuntu@ip-172-31-39-216.eu-west-1.compute.internal>
2018-05-14 12:26:01 +0000

    p,dks

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install -y nginx docker.io vim lvm2
-sudo chmod 666 /var/run/docker.sock
+sudo chmod 777 /var/run/docker.sock
 sudo apt-get install awscli -y
 sudo apt-get install tmux -y
+
